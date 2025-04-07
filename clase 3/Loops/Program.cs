7abba8d97418f8643ed7a17e233628bce8911e46
Paulo Castro <pcastro286@gmail.com>
2025-04-06 21:04:27 -0500

    tabla de multiplicacion y patron usando for

--- clase 3/Loops/Program.cs
+++ clase 3/Loops/Program.cs
@@ -1,21 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// Patron usando while
+Console.WriteLine("Patron usando while:");
 int i = 1;
 while (i <= 5)
 {
     int j = 1;
-    while (j <= 5)
+    while (j <= i)
     {
-        if (j <= i)
-        {
-            Console.Write(j + " ");
-        }
-        else
-        {
-            Console.Write("* ");
-        }
+        Console.Write(j + " ");
         j++;
     }
     Console.WriteLine();
     i++;
+}
+// Tabla de multiplicacion de un numero
+Console.WriteLine("Ingresar numero:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Multiplication table for {number}:");
+
+for (int i = 1; i <= 12; i++)
+{
+    Console.WriteLine($"{number} x {i} = {number * i}");
+}
+
+
+
+//Patron de numero usando un for
+Console.WriteLine("Patron usando for:");
+
+for (int i = 1; i <= 5; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write(j + " ");
+    }
+    Console.WriteLine();
 }
